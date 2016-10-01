b8719d39955ad55d302513ccdaa78b4fb87357dd
yeongwei <ywlai@origineit.com>
2016-10-01 14:15:13 +0800

    Initial commit

---
+++ app/src/main/java/mooc/vandy/java4android/buildings/logic/Cottage.java
@@ -0,0 +1,12 @@
+package mooc.vandy.java4android.buildings.logic;
+
+/**
+ * This is the cottage class file.  It is a subclass of House.
+ */
+public class Cottage 
+       extends House {
+       
+    // TODO - Put your code here.
+    
+}
+
